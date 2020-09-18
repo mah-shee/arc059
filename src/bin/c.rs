c2b24f0b5d321325de8eab75b13b7d8dfb3d9cd8
masa <masatoshi.sboard@gmail.com>
2020-09-18 14:58:29 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,22 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::cmp::min;
 #[fastout]
 fn main() {
     input! {
+        n: isize,
+        a: [isize; n]
     }
+    let a_sum = a.iter().sum::<isize>();
+    let a_ave = a_sum / n - 1;
+    let a_pow = a.iter().map(|x| x * x).sum::<isize>();
+    let mut score = std::isize::MAX;
+    for i in 0..3 {
+        score = min(
+            score,
+            a_pow - 2 * a_sum * (a_ave + i) + n * (a_ave + i) * (a_ave + i),
+        );
+    }
+    println!("{}", score);
 }
